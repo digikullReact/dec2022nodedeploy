1eb8217c8d5aedfb887c4d05ada7ddbd5b20d918
shubh <shubhamdixit863@gmail.com>
2023-08-12 19:22:24 +0530

    Api tests

--- test/routes/data.test.js
+++ test/routes/data.test.js
@@ -1,24 +1,26 @@
-const chai=require("chai");
-const chaiHttp=require("chai-http");
-
-const app=require("../../app");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../app");
 
 chai.use(chaiHttp);
 
-describe("Test Root router ,main route",function(){
-    describe(" Root route test ,/",function(){
-        it("should return hi there",function(done){
+describe("Test Root router, main route", function() {
+    describe("Root route test, /", function() {
+        it("should return hi there", function(done) {
             chai.request(app)
             .get("/")
-            .end((err,res)=>{
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
                 console.log(res.body);
-                chai.assert.equal(res.body,"hi there")
+                chai.assert.equal(res.text, "hi there"); // assuming your response body is like { message: "hi there" }
                 done();
-            })
+            });
+        });
+    });
+});
 
-        })
-
-    })
 
 
-})
