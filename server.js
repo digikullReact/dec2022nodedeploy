75c87cc04a04eda1da3c9458fc365d63ccfc4b08
shubh <shubhamdixit863@gmail.com>
2023-08-12 20:19:14 +0530

    socket io

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const app=require("./app");
 const {sequelize} = require("./repository/mysql");
 const mongoose = require('mongoose');
 
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+      origin: ["http://localhost:3000"]
+    }
+  });
 
 const dbConnect=()=>{
     return  mongoose.connect('mongodb+srv://dbuser:E1yXLwftoemYcA42@cluster0.jkkvfwg.mongodb.net/?retryWrites=true&w=majority')
@@ -19,9 +29,10 @@ if (process.argv[2]=="mysql"){
     mysqlConnect().then(data=>{
         console.log("Connected with Mysql-------")
     
-    app.listen("8080", () => {
-        console.log("Server running at port 8080")
-    })
+        httpServer.listen(8080,()=>{
+            console.log("Socket server running")
+        });
+
     
     }).catch(err=>{
         console.log(err);
@@ -31,9 +42,9 @@ if (process.argv[2]=="mysql"){
     dbConnect().then(data=>{
         console.log("Connected with Mongdob----")
     
-    app.listen("8080", () => {
-        console.log("Server running at port 8080")
-    })
+        httpServer.listen(8080,()=>{
+            console.log("Socket server running")
+        });
     
     }).catch(err=>{
         console.log(err);
@@ -44,20 +55,18 @@ if (process.argv[2]=="mysql"){
 
 
 
- /*
-dbConnect().then(data=>{
-    console.log("Connected with Mongdob----")
-
-app.listen("8080", () => {
-    console.log("Server running at port 8080")
-})
-
-}).catch(err=>{
-    console.log(err);
-})
 
 
-*/
+io.on("connection", (socket) => {
+    console.log("Client connected");
+
+    socket.on("message",(data)=>{
+       // console.log(data);
+       socket.broadcast.timeout(5000).emit("serversent",data)    
+    
+    })
+  // ...
+});
 
 
 
